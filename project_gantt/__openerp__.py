f7dca1d6835128bf3868c94c7ec4f57999005bad
Stefan Becker <ich@funbaker.de>
2016-06-18 16:25:51 +0200

    project gantt view

---
+++ project_gantt/__openerp__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Project Gantt',
+	'description': 'Adding a gantt view to projects',
+	'author': 'Stefan Becker <ich@funbaker.de>',
+	'depends': ['project', 'web_gantt'],
+	'data': ['views.xml']
+}
